26eaccead8c36baa0752c9a3fee56a0d56e48607
codeprerana <prerana_sheth@yahoo.com>
2017-08-02 14:41:52 -0700

    committttttttttttttt

---
+++ workspace/dupl_test/src/dupl_test_pac/dupl_test_git.java
@@ -0,0 +1,34 @@
+package dupl_test_pac;
+
+import java.util.ArrayList;
+
+public class dupl_test_git {
+
+	public static void main(String[] args) {
+		int a[]={4,5,5,8,4,4,4,4};
+		ArrayList<Integer> uni=new ArrayList<Integer>();
+		ArrayList<Integer> dup=new ArrayList<Integer>();
+		ArrayList<Integer> temp=new ArrayList<Integer>();
+		int k;
+
+		for(int i=0; i<a.length; i++)
+		{ 	 k=0;
+			if (!temp.contains(a[i]))
+				{ for(int j=i+1; j<a.length; j++)
+						{if(a[i]==a[j])
+							k=k+1;
+						}
+				}
+			if (k==0)
+				uni.add(a[i]);
+			if (k==1) 
+				dup.add(a[i]);
+			
+		}
+		System.out.println("Duplicates No.:"+dup);
+		System.out.println("Uniques:"+uni);
+
+
+	}
+
+}
